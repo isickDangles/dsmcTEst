aa6ba914b3529cd41bdabcb81e1db4d454c648fd
unknown <smith2941@marshall.edu>
2024-02-01 12:26:58 -0500

    WIP Survey Creation Page

--- client/src/CreateSurveyPage.js
+++ client/src/CreateSurveyPage.js
@@ -1,84 +1,247 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
 import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
 import Select from '@mui/material/Select';
-import Tooltip from '@mui/material/Tooltip';
-export default function BasicTextFields() {
+import MenuItem from '@mui/material/MenuItem';
+import Radio from '@mui/material/Radio';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import { Tooltip } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-    const [office, setOffice] = React.useState('');
-    const [program, setProgram] = React.useState('');
 
-    const changeOffice = (event) => {
-        setOffice(event.target.value);
+export default function SurveyCreationPage() {
+    const [surveyName, setSurveyName] = useState('');
+    const [creatingQuestion, setCreatingQuestion] = useState(false);
+    const [questionType, setQuestionType] = useState('');
+    const [questionText, setQuestionText] = useState('');
+    const [questionOptions, setQuestionOptions] = useState(['']); // Define questionOptions and its setter
+    const [questions, setQuestions] = useState([]);
+    const [surveyCreated, setSurveyCreated] = useState('');
+    const [isSurveyComplete, setIsSurveyComplete] = useState(false);
 
+
+    const handleSurveyNameChange = (event) => {
+        setSurveyName(event.target.value);
     };
-    const changeProgram = (event) => {
-        setProgram(event.target.value);
 
-    };
+    const handleDeleteQuestion = (index) => {
+        setQuestions(questions.filter((_, qIndex) => qIndex !== index)); //Delete  
+    }
+        const handleCreateSurvey = () => {
+            setSurveyCreated(true);
+        };
 
-    return (
-        <Box
-            component="form"
-            sx={{
-                '& > :not(style)': { m: 1, width: '75ch' },
-            }}
-            noValidate
-            autoComplete="off"
-            display="flex"
-            flexDirection='column'
-            minHeight='100vh'
-        >
+        const handleCreateQuestionClick = () => {
+            setCreatingQuestion(true);
+        };
+
+        const handleQuestionTypeChange = (event) => {
+            setQuestionType(event.target.value);
+        };
+
+        const handleQuestionTextChange = (event) => {
+            setQuestionText(event.target.value);
+        };
+
+        const handleOptionChange = (index, event) => {
+            const updatedOptions = questionOptions.map((option, i) => {
+                if (i === index) {
+                    return event.target.value;
+                }
+                return option;
+            });
+            setQuestionOptions(updatedOptions);
+        };
+
+        const handleAddOption = () => {
+            setQuestionOptions([...questionOptions, '']);
+        };
+
+        const handleCompleteQuestion = () => {
+            if (questionType === 'multipleChoice' && questionOptions.some(option => option === '')) {
+                alert('Please fill in all options for the multiple choice question.');
+                return;
+            }
+
+            const newQuestion = {
+                type: questionType,
+                text: questionText,
+                options: questionType === 'multipleChoice' ? questionOptions :
+                    questionType === 'likertScale' ? Array.from({ length: 10 }, (_, i) => (i + 1).toString()) :
+                        undefined,
+
+            };
+            setQuestions([...questions, newQuestion]);
+            setCreatingQuestion(false);
+            setQuestionType('');
+            setQuestionText('');
+            setQuestionOptions(['', '']);
+        };
+
+        const handleCompleteSurvey = () => {
+            if (questions.length === 0) {
+                alert('Please add at least one question before completing the survey.');
+                return;
+            }
+            setSurveyCreated(false);
+            setIsSurveyComplete(true);
+        };
+
+        return (
+            <Box
+                component="form"
+                sx={{ '& .MuiTextField-root': { m: 1 }, '& .MuiButton-root': { m: 1 }, maxWidth: '500px', margin: 'auto' }}
+                noValidate
+                autoComplete="off"
+            >
+                {!surveyCreated && !isSurveyComplete && (
+                    <>
+                        <TextField
+                            fullWidth
+                            id="outlined-basic"
+                            label="Survey Name"
+                            variant="outlined"
+                            value={surveyName}
+                            onChange={handleSurveyNameChange}
+                        />
+                        <Button variant="contained" color="primary" onClick={handleCreateSurvey}>
+                            Create Survey
+                        </Button>
+                    </>
+                )}
+
+                {surveyCreated && surveyName && (
+                    <Box sx={{ textAlign: 'center', width: '100%' }}>
+                        <h2>{surveyName}</h2>
+                    </Box>
+                )}
+                {/* This is to display the title of the survey at the top as the user makes it */}
 
 
-            <div>
+                {surveyCreated && (
+                    <>
+                        {questions.map((question, index) => (
+                            <Box key={index} sx={{ marginBottom: 2, display: 'flex', alignItems: 'center' }}>
+                                <Tooltip title="Delete">
+                                    <IconButton aria-label="delete" size="small" onClick={() => handleDeleteQuestion(index)}>
+                                        <DeleteIcon fontSize="small" />
+                                    </IconButton>
+                                </Tooltip>
+                                <FormControl fullWidth sx={{ mr: 2 }}>
+                                    <TextField
+                                        label={`Question ${index + 1}`}
+                                        variant="outlined"
+                                        value={question.text}
+                                        InputProps={{ readOnly: true }}
+                                    />
+                                </FormControl>
+                                <Box sx={{ fontWeight: 'bold' }}>
+                                    {question.type === 'trueFalse' ? 'True/False' :
+                                        question.type === 'multipleChoice' ? 'Multiple Choice' :
+                                            question.type === 'likertScale' ? 'Likert Scale (1-10)' :
+                                                'Unknown Type'}
+                                </Box>
+                            </Box>
+                        ))}
+
+                        {!creatingQuestion && (
+                            <Box sx={{ textAlign: 'center', width: '100%' }}>
+                                <Button variant="contained" onClick={handleCreateQuestionClick}>
+                                    Add New Question
+                                </Button>
+                            </Box>
+                        )}
+
+                        {creatingQuestion && (
+                            <Box sx={{ marginTop: 2 }}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="question-type-label">Question Type</InputLabel>
+                                    <Select
+                                        labelId="question-type-label"
+                                        id="question-type-select"
+                                        value={questionType}
+                                        label="Question Type"
+                                        onChange={handleQuestionTypeChange}
+                                    >
+                                        <MenuItem value="trueFalse">True/False</MenuItem>
+                                        <MenuItem value="multipleChoice">Multiple Choice</MenuItem>
+                                        <MenuItem value="likertScale">Likert Scale (1-10)</MenuItem>
+                                        {/* ... other question types */}
+                                    </Select>
+                                </FormControl>
+                                <TextField
+                                    fullWidth
+                                    label={`Question ${questions.length + 1}`}
+                                    variant="outlined"
+                                    value={questionText}
+                                    onChange={(e) => setQuestionText(e.target.value)}
+                                    sx={{ marginTop: 2 }}
+                                />
+                                {questionType === 'multipleChoice' && questionOptions.map((option, index) => (
+                                    <TextField
+                                        key={index}
+                                        fullWidth
+                                        label={`Option ${index + 1}`}
+                                        value={option}
+                                        onChange={(e) => handleOptionChange(index, e)}
+                                        margin="normal"
+                                    />
+                                ))}
+                                {questionType === 'multipleChoice' && (
+                                    <Button onClick={handleAddOption} variant="contained" sx={{ mt: 1 }}>
+                                        Add Option
+                                    </Button>
+                                )}
 
 
-                <p class="center">
-                    <TextField id="outlined-basic" label="Survey Name" variant="outlined" />
-                </p>
+                                {/*This is space for more question types*/}
 
-                <p>
-                <Tooltip title="Select the office this mission falls under" placement="right">
-                    <FormControl fullWidth >
-                        <InputLabel id="demo-simple-select-label">Office Mission</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={office}
-                            label="Office Mission"
-                            onChange={changeOffice}
-                        >
-                            <MenuItem value={10}>DSMMCX</MenuItem>
-                            <MenuItem value={20}>DSPC</MenuItem>
-                            <MenuItem value={30}>CELRH-PM-PD</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Tooltip>
-                </p>
-                <p>
-                <Tooltip title="Enter the official Corps Project Name" placement="right">
-                    <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label">Mission, Function, Program or Project</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={program}
-                            
-                            label="Mission, Function, Program or Project"
-                            onChange={changeProgram}
-                        >
-                            <MenuItem value={10}>Ex1</MenuItem>
-                            <MenuItem value={20}>Ex2</MenuItem>
-                            <MenuItem value={30}>Ex3</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Tooltip>
-                </p>
-            </div>
-        </Box>
-    );
-}
+                                <Button onClick={handleCompleteQuestion} variant="contained" sx={{ mt: 2 }}>
+                                    Save Question
+                                </Button>
+                            </Box>
+                        )}
+                    </>
+                )}
+
+                {isSurveyComplete && (
+                    <>
+                        <h2>{surveyName}</h2>
+                        {questions.map((question, index) => (
+                            <Box key={index}>
+                                <FormControl component="fieldset" sx={{ marginTop: 2 }}>
+                                    <FormLabel component="legend">{`Question ${index + 1}: ${question.text}`}</FormLabel>
+                                    {question.type === 'multipleChoice' ? (
+                                        <RadioGroup row name={`question_${index}`}>
+                                            {question.options.map((option, optionIndex) => (
+                                                <FormControlLabel
+                                                    key={optionIndex}
+                                                    value={option}
+                                                    control={<Radio />}
+                                                    label={option}
+                                                />
+                                            ))}
+                                        </RadioGroup>
+                                    ) : (
+                                        <RadioGroup row name={`question_${index}`}>
+                                            <FormControlLabel value="true" control={<Radio />} label="True" />
+                                            <FormControlLabel value="false" control={<Radio />} label="False" />
+                                        </RadioGroup>
+                                    )}
+                                </FormControl>
+                            </Box>
+                        ))}
+                        <Button variant="contained" color="primary" sx={{ marginTop: 2 }}>
+                            Submit Answers
+                        </Button>
+                    </>
+                )}
+            </Box>
+        );
+    }
